5a7b83e945e0f59d2db130834b501fa8646f660d
Ameer <ameerabdrabo7@gmail.com>
2024-09-06 17:02:30 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import './index.css';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Books from './pages/Books';
-import { books } from './data'
+import "./index.css";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import Cart from "./pages/Cart";
+import React, { useEffect, useState } from "react";
+import BookInfo from "./pages/Bookinfo";
+
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book])
+  }
+
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
+
   return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} />} />
-      <Route path="/books/" />
-      <Footer />
-    </div>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route
+          path="/books/:id"
+          render={() => <BookInfo books={books} addToCart={addToCart} cart={cart} />}
+        />
+        <Route path="/cart" render={() => <Cart books={books} cart={cart} />} />
+        <Footer />
+      </div>
     </Router>
   );
 }
